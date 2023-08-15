8f4af85e9a5398c2b03c3040778cddb5a6bd1c41
nham-nguyen <nham.nguyen@uptempo-global.com>
2023-08-15 13:03:52 +0700

    update

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -15,7 +15,7 @@ userRouter.post(
   asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-      
+
     if (user && (await user.matchPassword(password))) {
       res.json({
         _id: user._id,
@@ -37,7 +37,6 @@ userRouter.post(
   "/register",
   asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
-
     const userExists = await User.findOne({ email });
 
     if (userExists) {
@@ -90,9 +89,10 @@ userRouter.get(
 
 // UPDATE PROFILE
 userRouter.put(
-  "/profile",
+  "/update-profile",
   protect,
   asyncHandler(async (req, res) => {
+    console.log("req req req        ",req)
     const user = await User.findById(req.user._id);
 
     if (user) {
