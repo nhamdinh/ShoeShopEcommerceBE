ecc60c104ab9c2b41e0e3b7edd8a745fd872c536
nham dinh <nham.nguyen@uptempo-global.com>
2023-09-13 21:53:58 +0700

    update controller

--- src/api/v1/routes/v1/categoryRoutes.js
+++ src/api/v1/routes/v1/categoryRoutes.js
@@ -1,163 +1,42 @@
 const express = require("express");
-const asyncHandler = require("express-async-handler");
 const { protect, admin } = require("./../../Middleware/AuthMiddleware");
 
-const Category = require("./../../Models/CategoryModel");
-const Brand = require("./../../Models/BrandModel");
-const { PAGE_SIZE } = require("./../../utils/constant");
+const {
+  getAllCategory,
+  getAllCategoryByAdmin,
+  deleteCategoryById,
+  createCategory,
+  getAllBrand,
+  getAllBrandByAdmin,
+  deleteBrandById,
+  createBrand,
+} = require("../../controller/category.controller");
 
 const categoryRoutes = express.Router();
 
 // GET ALL CATEGORY
-categoryRoutes.get(
-  "/get-all",
-  asyncHandler(async (req, res) => {
-    const page = Number(req.query.pageNumber) || 1;
-    const keyword = req.query.keyword
-      ? {
-          name: {
-            $regex: req.query.keyword,
-            $options: "i",
-          },
-        }
-      : {};
-    const count = await Category.countDocuments({ ...keyword });
-    const categorys = await Category.find({ ...keyword })
-      .limit(PAGE_SIZE)
-      .skip(PAGE_SIZE * (page - 1))
-      .sort({ _id: -1 });
-    res.json({ count, categorys, page, pages: Math.ceil(count / PAGE_SIZE) });
-  })
-);
+categoryRoutes.get("/get-all", getAllCategory);
 
 // ADMIN GET ALL CATEGORY WITHOUT SEARCH AND PAGINATION
-categoryRoutes.get(
-  "/all-admin",
-  protect,
-  asyncHandler(async (req, res) => {
-    const categorys = await Category.find({ deletedAt: null }).sort({
-      createdAt: -1,
-    });
-    res.json({ categorys });
-  })
-);
+categoryRoutes.get("/all-admin", protect, getAllCategoryByAdmin);
 
 // DELETE CATEGORY
-categoryRoutes.post(
-  "/delete/:id",
-  protect,
-  admin,
-  asyncHandler(async (req, res) => {
-    const category = await Category.findById(req.params.id);
-    if (category) {
-      category.deletedAt = Date.now();
-      await category.save();
-      res.json({ message: "Category deleted" });
-    } else {
-      res.status(404).json({ message: "Category not Found" });
-      throw new Error("Category not Found");
-    }
-  })
-);
+categoryRoutes.post("/delete/:id", protect, admin, deleteCategoryById);
 
 // CREATE CATEGORY
-categoryRoutes.post(
-  "/create",
-  protect,
-  admin,
-  asyncHandler(async (req, res) => {
-    const { category } = req.body;
-    const categoryExist = await Category.findOne({ category });
-    if (categoryExist) {
-      res.status(400).json({ message: "Category name already exist" });
-
-      throw new Error("Category name already exist");
-    } else {
-      const category1 = new Category({
-        category,
-        user: req.user._id,
-      });
-      if (category) {
-        const createCategory = await category1.save();
-        res.status(201).json(createCategory);
-      } else {
-        res.status(400).json({ message: "Invalid category data" });
-        throw new Error("Invalid category data");
-      }
-    }
-  })
-);
+categoryRoutes.post("/create", protect, admin, createCategory);
 
 /* --------------------------------------------------------------------------------------------------------------- */
 // GET ALL BRAND
-categoryRoutes.get(
-  "/get-all-brands",
-  asyncHandler(async (req, res) => {
-    const brands = await Brand.find({ deletedAt: null }).sort({
-      createdAt: -1,
-    });
-    res.json({ brands });
-  })
-);
+categoryRoutes.get("/get-all-brands", getAllBrand);
 
 // ADMIN GET ALL BRAND WITHOUT SEARCH AND PAGINATION
-categoryRoutes.get(
-  "/all-admin/brand",
-  protect,
-  admin,
-  asyncHandler(async (req, res) => {
-    const brands = await Brand.find({ deletedAt: null }).sort({
-      createdAt: -1,
-    });
-    res.json({ brands });
-  })
-);
+categoryRoutes.get("/all-admin/brand", protect, admin, getAllBrandByAdmin);
 
 // DELETE BRAND
-categoryRoutes.post(
-  "/delete/:id/brand",
-  protect,
-  admin,
-  asyncHandler(async (req, res) => {
-    const brands = await Brand.findById(req.params.id);
-    if (brands) {
-      brands.deletedAt = Date.now();
-      await brands.save();
-      res.json({ message: "Brand deleted" });
-    } else {
-      res.status(404).json({ message: "Brand not Found" });
-      throw new Error("Brand not Found");
-    }
-  })
-);
+categoryRoutes.post("/delete/:id/brand", protect, admin, deleteBrandById);
 
 // CREATE BRAND
-categoryRoutes.post(
-  "/create-brand",
-  protect,
-  admin,
-  asyncHandler(async (req, res) => {
-    const { brand, image } = req.body;
-    const brandExist = await Brand.findOne({ brand });
-    if (brandExist) {
-      res.status(400).json({ message: "Brand name already exist" });
-
-      throw new Error("Brand name already exist");
-    } else {
-      const brand1 = new Brand({
-        brand,
-        image,
-        user: req.user._id,
-      });
-      if (brand) {
-        const createBrand = await brand1.save();
-        res.status(201).json(createBrand);
-      } else {
-        res.status(400).json({ message: "Invalid brand data" });
-        throw new Error("Invalid brand data");
-      }
-    }
-  })
-);
+categoryRoutes.post("/create-brand", protect, admin, createBrand);
 
 module.exports = categoryRoutes;
