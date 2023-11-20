b138f7aafbbafe97646e2c8983bcb334bc16831e
nham dinh <nham.nguyen@uptempo-global.com>
2023-11-20 16:55:59 +0700

    update compression

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require("./app");
 const server = require("./socket");
 const logger = require("./src/api/v1/log");
 
+/* chay voi bao nhieu core */
 process.env.UV_THREADPOOL_SIZE = Math.floor(Number(os.cpus().length) * 0.8);
 logger.info(
   "Number of UV_THREADPOOL_SIZE ::: " +
--- src/api/v1/dataMock/Users.js
+++ src/api/v1/dataMock/Users.js
@@ -5,14 +5,14 @@ const users = [
     name: "Admin",
     email: "admin@example.com",
     phone: "0680225733",
-    password: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("123456", 8),
     isAdmin: true,
   },
   {
     name: "User",
     email: "user@example.com",
     phone: "0890845016",
-    password: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("123456", 8),
   },
 ];
 
