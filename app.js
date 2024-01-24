2f72ca8805a8bb9a34be77591cc889fe59d066e0
nham dinh <nham.nguyen@uptempo-global.com>
2024-01-24 15:12:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 // app.use(cookieParser("randomsecretstring"));
 app.use(cookieParser());
-
+app.use(cors());
 app.use(
   cors({
     origin: "*",
