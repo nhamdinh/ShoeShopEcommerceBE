97a71714045468dc10569de53a0dd01df5626d26
nham-nguyen <nham.nguyen@uptempo-global.com>
2024-04-18 20:25:03 +0700

    restart shopnode-be

--- src/api/v1/utils/constant.js
+++ src/api/v1/utils/constant.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   PAGE_SIZE: 6,
-  strCompression: 'hello world 44',
+  strCompression: 'hello world 11',
   URL_SERVER: process.env.URL_SERVER,
   COOKIE_REFRESH_TOKEN: "refreshToken",
   PRODUCT_TYPE: ["electronic", "clothing", "furniture","household"],
