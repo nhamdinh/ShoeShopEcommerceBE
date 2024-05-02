3a1e0f781a066f914ccdb8005a8bd83491637bba
nham-nguyen <nham.nguyen@uptempo-global.com>
2024-05-02 17:35:26 +0700

    up

--- src/api/v1/utils/constant.js
+++ src/api/v1/utils/constant.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   PAGE_SIZE: 6,
-  strCompression: "hello world 11",
+  strCompression: "hello world 22",
   URL_SERVER: process.env.URL_SERVER,
   COOKIE_REFRESH_TOKEN: "refreshToken",
   PRODUCT_TYPE: ["electronic", "clothing", "furniture", "household"],
